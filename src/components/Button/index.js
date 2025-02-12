a2a855e444738f0cc6d04d3117a38e1cf73ea847
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-12 18:13:47 -0300

    Criado componente Button

---
+++ src/components/Button/index.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+    text-align: center;
+    font-size: 16px;
+    color: #fff;
+    width: max-content;
+    min-width: 180px;
+    border: 2px solid ${ props => props.setBorderColor || '#fff' };
+    background-color: ${ props => props.setBackgroundColor || 'transparent' };
+    padding: 10px 20px;
+    border-radius: 10px;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    display: block;
+`
