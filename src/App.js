97fddef0b400a42f83594f248588e64c983673a8
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-12 16:38:30 -0300

    Personaliza Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header 
+        logoPath="/img/logo3.png" 
+        logoAlt="Logo da UpFind" 
+        linkLogin="#"
+        linkCadastro="#"
+      />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,59 @@
 import styled from "styled-components";
 
-const headerContainer = styled.header`
-    width: 100%;
-    padding: 20px;
-    background-color: #000;
+const HeaderContainer = styled.header`
+    max-width: 100%;
+    padding: 10px 20px;
+    background-color: #121212;
     display: flex;
     align-items: center;
     justify-content: space-between;
-`
+`;
+
+const Logo = styled.img`
+    width: 120px; /* Ajuste o tamanho conforme necessário */
+    height: auto; /* Mantém a proporção */
+    cursor: pointer; /* Exemplo: torna a logo clicável */
+    
+    /* Exemplo: adicionando um efeito ao passar o mouse */
+    &:hover {
+        opacity: 0.8;
+        transition: opacity 0.3s ease-in-out;
+    }
+`;
+
+const LinksContainer = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 30px;
+`;
+
+const LinkLogin = styled.a`
+    color: #fff;
+    text-decoration: none;
+    font-weight: bold;
+    transition: color 0.3s ease;
+`;
+
+const LinkCadastro = styled.a`
+    color: #fff;
+    text-decoration: none;
+    font-weight: bold;
+    transition: color 0.3s ease;
+    padding: 8px 30px;
+    border-radius: 15px;
+    border: 2px solid #fff;
+`;
 
 const Header = (props) => {
     return (
-        <headerContainer>
-            <img src={ props.logoPath } alt={ props.logoSrc } />
-            <div>
-                <a href={ props.linkLogin }>Entrar</a>
-                <a href={ props.linkCadastro }>Cadastre-se</a>
-            </div>
-        </headerContainer>
-    )
-}
+        <HeaderContainer>
+            <Logo src={props.logoPath} alt={props.logoSrc} />
+            <LinksContainer>
+                <LinkLogin href={props.linkLogin}>Entrar</LinkLogin>
+                <LinkCadastro href={props.linkCadastro}>Cadastre-se</LinkCadastro>
+            </LinksContainer>
+        </HeaderContainer>
+    );
+};
 
 export default Header;
