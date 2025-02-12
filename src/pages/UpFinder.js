00c0ab7a62019e43a033ac8d269ff5cb219df909
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-12 18:03:11 -0300

    Adiciona página UpFinder

--- src/pages/UpFinder.js
+++ src/pages/UpFinder.js
@@ -1,5 +1,18 @@
+import Header from "../components/Header"
+import Welcome from "../components/Welcome"
+
 const UpFinder = () => {
-    return <h1>Página UpFinder</h1>
+    return (
+        <div className="App">
+            <Header
+                logoPath="/img/iv/logo3.png"
+                logoAlt="Logo da UpFind"
+                linkLogin="#"
+                linkCadastro="#"
+            />
+            <Welcome />
+        </div>
+    )
 }
 
 export default UpFinder
