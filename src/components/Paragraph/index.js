f551fb042e41712eaa9d89be0976800e93d6958d
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-12 17:21:32 -0300

    Adiciona padding em Title e Paragraph

--- src/components/Paragraph/index.js
+++ src/components/Paragraph/index.js
@@ -5,4 +5,5 @@ export const Paragraph = styled.p`
     color: ${ props => props.color || '#000' };
     text-align: ${ props => props.align || 'left' };
     font-size: ${ props => props.size || '16px' }; 
+    padding: 0 20px;
 `
--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -5,4 +5,5 @@ export const Title = styled.h2`
     color: ${ props => props.color || '#000' };
     text-align: ${ props => props.align || 'center' };
     margin-bottom: 10px;
+    padding: 0 20px;
 `
