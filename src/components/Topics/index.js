3f0a6c8794200b363b3689dbdd2d2aa732aca683
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-13 09:52:24 -0300

    Altera padding de Topics

--- src/components/Topics/index.js
+++ src/components/Topics/index.js
@@ -6,7 +6,7 @@ const TopicsContainer = styled.section`
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     gap: 64px 32px;
-    padding: 40px 20px;
+    padding: 70px 20px;
     background-color: var(--color-black);
 
     @media (max-width: 768px) {
