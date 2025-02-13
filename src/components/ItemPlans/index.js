4ca288c09e63c9ee3eb511c5cb70adf4f259bcee
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-13 13:05:40 -0300

    Cria componente ItemPlans

---
+++ src/components/ItemPlans/index.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { Paragraph } from "../Paragraph";
+
+const ItemPlansContainer = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    gap: 16px;
+`
+
+const ImgIcon = styled.img`
+    width: 30px;
+    filter: invert(0);
+`
+
+const ItemPlans = (props) => {
+    const colorText = props.icon === "x" ? 'var(--color-black)' : 'var(--color-white)'
+    const filterIcon = props.icon === "x" ? 0 : 1;
+
+    return (
+        <ItemPlansContainer>
+            <ImgIcon style={{ filter: `invert(${filterIcon})` }} src={`/img/icons/${props.icon}.svg`} alt="Ícone" />
+            <Paragraph color={colorText}>{props.text}</Paragraph>
+        </ItemPlansContainer>
+    )
+}
+
+export default ItemPlans;
