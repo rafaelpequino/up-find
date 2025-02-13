2e8db0b3531e9cac6438bfa327fc88fef5b1abab
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-12 21:00:30 -0300

    Criado componente Topics

---
+++ src/components/Topics/index.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import ItemTopics from "../ItemTopics";
+
+const TopicsContainer = styled.section`
+    max-width: 100%;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 64px 32px;
+    padding: 60px 20px;
+    background-color: #000;
+
+    @media (max-width: 768px) {
+        grid-template-columns: repeat(1, 1fr);
+    }
+`
+
+const Topics = () => {
+    return (
+        <TopicsContainer>
+            <ItemTopics 
+                icon="document"
+                text="Recebe até 3 orçamentos de especialistas"
+            />
+            <ItemTopics 
+                icon="web"
+                text="Tudo feito 100% online, sem sair de casa"
+            />
+            <ItemTopics 
+                icon="lock"
+                text="Garanta sua segurança com o modo UpSafety"
+            />
+        </TopicsContainer>
+    )
+}
+
+export default Topics;
