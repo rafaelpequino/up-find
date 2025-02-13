867980d296b500f3632bbffbb1525169a42b5c5a
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-13 11:07:14 -0300

    Cria estilização e responsividade em Process

--- src/components/Process/index.js
+++ src/components/Process/index.js
@@ -1,5 +1,30 @@
 import styled from "styled-components";
 import { Title } from "../Title";
+import StepProcess from "../StepProcess";
+
+const steps = [
+    {
+        title: '1 - Criação do projeto',
+        desc: 'Você irá preencher um formulário fornecendo todas as informações necessárias sobre o projeto',
+        align: 'right'
+    },
+    {
+        title: '2 - Direcionamento',
+        desc: 'Nosso algoritmo irá selecionar os desenvolvedores que mais se enquadram em suas necessidades',
+        align: 'left'
+    },
+    {
+        title: '3 - Contato e Orçamento',
+        desc: 'Até 3 profissionais entrarão em contato para fornecer informações e orçamentos',
+        align: 'right'
+    },
+    {
+        title: '4 - Fechamento',
+        desc: 'Após decidir qual o profissional que irá realizar o serviço, você poderá escolher uma das formas abaixo para finalizar a contratação',
+        align: 'left'
+    }
+    
+]
 
 const ProcessContainer = styled.section`
     max-width: 100%;
@@ -8,10 +33,40 @@ const ProcessContainer = styled.section`
     padding: 70px 20px;
 `
 
+const StepByStep = styled.div`
+    width: 100%;
+    max-width: 800px;
+    margin: 40px auto;
+    position: relative;
+`
+
+const CenterLine = styled.div`
+    position: absolute;
+    top: 75px;
+    left: 50%;
+    width: 2px;
+    height: 78%;
+    background-color: var(--color-white);
+
+    @media (max-width: 768px) {
+        left: 27px;
+    }
+`
+
 const Process = () => {
     return (
         <ProcessContainer>
             <Title size="50px" color="var(--color-white)" spanColor="var(--color-sky-1)" align="center">Cada passo do <span>processo</span> é pensado em você</Title>
+            <StepByStep>
+                { steps.map( step => (
+                    <StepProcess 
+                        titleColor="var(--color-sky-1)" 
+                        align={step.align} 
+                        titleText={step.title} 
+                        desc={step.desc} /> 
+                )) }
+                <CenterLine></CenterLine>
+            </StepByStep>
         </ProcessContainer>
     )
 }
---
+++ src/components/StepProcess/index.js
@@ -0,0 +1,79 @@
+import styled from "styled-components";
+import { Paragraph } from "../Paragraph";
+import { useState, useEffect } from "react";
+
+const StepProcessContainer = styled.div`
+    width: 100%;
+    display: grid;
+    grid-template-columns: calc(50% - 45px) 50px calc(50% - 45px);
+    align-items: center;
+    gap: 20px;
+    padding: 32px 0;
+    margin: auto;
+
+    @media (max-width: 768px) {
+        grid-template-columns: 50px auto;
+    }
+`;
+
+const AssistantDiv = styled.div`
+    display: block;
+
+    @media (max-width: 768px) {
+        display: none;
+    }
+`;
+
+const CircleStep = styled.div`
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    border: 2px solid var(--color-sky-1);
+    background-color: var(--color-black);
+    z-index: 10;
+
+    @media (max-width: 768px) {
+        display: block;
+    }
+`;
+
+const StepProcess = (props) => {
+    // Estado para alinhar os textos com base no tamanho da tela
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        // Adiciona o listener de resize
+        window.addEventListener('resize', handleResize);
+        
+        // Remove o listener ao desmontar o componente
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    const isRightAligned = !isMobile && props.align === 'right';
+    const paragraphAlign = isMobile ? 'left' : props.align;
+
+    return (
+        <StepProcessContainer>
+            {!isRightAligned && <AssistantDiv />}
+            {!isRightAligned && <CircleStep />}
+            
+            <div>
+                <Paragraph color={props.titleColor} align={paragraphAlign}>
+                    <strong>{props.titleText}</strong>
+                </Paragraph>
+                <Paragraph color="var(--color-white)" align={paragraphAlign}>
+                    {props.desc}
+                </Paragraph>
+            </div>
+
+            {isRightAligned && <CircleStep />}
+            {isRightAligned && <AssistantDiv />}
+        </StepProcessContainer>
+    );
+};
+
+export default StepProcess;
