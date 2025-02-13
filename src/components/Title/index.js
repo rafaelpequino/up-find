c880b3e3b90cfbc0c5e88d08834c84edfac11e8e
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-13 09:52:13 -0300

    Altera margin do Title

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -4,7 +4,7 @@ export const Title = styled.h2`
     font-size: ${ props => props.size || '32px' };
     color: ${ props => props.color || 'var(--color-black)' };
     text-align: ${ props => props.align || 'center' };
-    margin-bottom: 10px;
+    margin: 10px auto;
     padding: 0 20px;
     max-width: 800px;
     font-weight: 700;
