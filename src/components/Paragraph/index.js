2c710d3df9b7099f5c264c6fc0e44be32dc79e01
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-13 13:06:05 -0300

    Adiciona span ao Paragraph

--- src/components/Paragraph/index.js
+++ src/components/Paragraph/index.js
@@ -7,6 +7,10 @@ export const Paragraph = styled.p`
     font-size: ${ props => props.size || '16px' }; 
     padding: 0 20px;
 
+    & span {
+        color: var(--color-sky-1);
+    }
+
     @media (max-width: 768px) {
         font-size: 16px;
     }
